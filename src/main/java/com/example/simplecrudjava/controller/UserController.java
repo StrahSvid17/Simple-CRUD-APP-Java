61ba40a0bf1fdecb1b7be44d62407e9732a17c4f
admin <maximsushkin02@gmail.com>
2022-06-28 22:13:19 +0300

    Добавлена JPA структура для сущности

--- src/main/java/com/example/simplecrudjava/controller/UserController.java
+++ src/main/java/com/example/simplecrudjava/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.example.simplecrudjava.controller;
 
 public class UserController {
+
 }
--- src/main/java/com/example/simplecrudjava/model/User.java
+++ src/main/java/com/example/simplecrudjava/model/User.java
@@ -1,4 +1,33 @@
 package com.example.simplecrudjava.model;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity(name = "users_tbl")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id", "username"})
+@ToString(of = {"id", "username", "email", "address"})
 public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
 }
