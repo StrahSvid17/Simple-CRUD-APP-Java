29acc4758560d3d5b74563993f8d55a69244beac
admin <maximsushkin02@gmail.com>
2022-06-28 22:19:56 +0300

    Добавлен репозиторий для работы с основной сущностью

--- src/main/java/com/example/simplecrudjava/repository/UserRepository.java
+++ src/main/java/com/example/simplecrudjava/repository/UserRepository.java
@@ -1,4 +1,19 @@
 package com.example.simplecrudjava.repository;
 
-public class UserRepository {
+import com.example.simplecrudjava.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Query("update users_tbl u set u.username = ?1, u.email = ?2 where u.id = ?3")
+    void updateUser(String username, String email, Long id);
 }
