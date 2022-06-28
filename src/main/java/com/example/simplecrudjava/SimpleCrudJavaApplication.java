c141c199ce85d0b718f45110f8bf0b72c0d1c902
admin <maximsushkin02@gmail.com>
2022-06-28 22:03:57 +0300

    init

---
+++ src/main/java/com/example/simplecrudjava/SimpleCrudJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplecrudjava/SimpleCrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
